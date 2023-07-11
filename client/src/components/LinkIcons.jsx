f5e35e5793c5dba11a6220f6fa4095e96333d2c0
Harshit <2105891@kiit.ac.in>
2023-07-12 00:27:55 +0530

    footer icon resized

--- client/src/components/LinkIcons.jsx
+++ client/src/components/LinkIcons.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const LinkIcons = ({url, imgSrc}) => {
+const LinkIcons = ({ url, imgSrc }) => {
   return (
-      <a href={url}>
-          <img src={imgSrc} alt="" className='xsm:w-[1.8rem] xsm:h-[1.8rem] md:w-[3.1rem] md:h-[3.1rem]' />
-      </a>
+    <a href={url}>
+      <img src={imgSrc} alt="" className='xsm:w-[2.7rem] xsm:h-[2.7rem] md:w-[3.1rem] md:h-[3.1rem]' />
+    </a>
   )
 }
 

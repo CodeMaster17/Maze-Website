6fda54e829ba4b2d778346c66f0c7d7f604ed3f7
Harshit <2105891@kiit.ac.in>
2023-07-11 18:49:33 +0530

    server folder structure updated

--- server/api/index.js
+++ server/api/index.js
@@ -13,6 +13,8 @@ app.use(router)
 // console.log("hello")
 
 app.get("/", (req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
     res.send("<h1>Welcome To Our API</h1>")
 })
 

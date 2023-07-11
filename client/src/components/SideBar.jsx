020444126e048af5e127832ef72ddbb79941691d
Harshit <2105891@kiit.ac.in>
2023-07-11 21:40:32 +0530

    sidebar icon path changed

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -4,10 +4,10 @@ const SideBar = ({ onClick }) => {
     return (
         <div className='absolute right-0 w-1/2 h-[100vh] bg-navbarBg z-50 top-0 flex justify-end' >
             <button className='left-[-5%] md:left-[-6.5%]  top-[5%] absolute ' onClick={onClick}>
-                <img src="../../public/sidebar-icon.svg" className='w-[3rem] h-[3rem] md:w-[6rem] md:h-[6rem] ' alt="" />
+                <img src="/sidebar-icon.svg" className='w-[3rem] h-[3rem] md:w-[6rem] md:h-[6rem] ' alt="close-button" />
             </button>
 
-            <div className='w-[80%] border-2 left-[5%] top-[10%]  mt-[20%] flex flex-col '>
+            <div className='w-[80%]  left-[5%] top-[10%]  mt-[20%] flex flex-col '>
                 <a className='text-[2rem] text-textWhite md:text-[3rem] font-[400] mt-2'>Home</a>
                 <a className='text-[2rem] text-textWhite md:text-[3rem] font-[400] mt-2'>Contact</a>
                 <a className='text-[2rem] text-textWhite md:text-[3rem] font-[400] mt-2'>Join Now</a>

586ba3ae04a7af9ddd2489d1c0fac14e8b1aa9ee
Harshit <2105891@kiit.ac.in>
2023-07-12 00:23:05 +0530

    footer corrected

--- client/src/Data/data.js
+++ client/src/Data/data.js
@@ -10,6 +10,6 @@ export const instagram_URL = ""
 export const facebook_URL = ""
 
 export const footerText = "Maze is a sports tech company that allows fans to invest in the talents they believe and own a part of their future success."
-export const address = "copyright © 2023 by Yaro Technology Pvt Ltd"
+export const address = "C-71, First Floor, Sector 63, Noida 201301"
 export const visionText = "Build your portfolio of athlete you believe in,invest in their training and own a part of their future earnings."
 export const investmentIdea = "Help the next big name of the Indian cricket team in their journey of greatness financially and emotionally."
